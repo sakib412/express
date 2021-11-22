c159964123602e333724aeecec9b0edf7bcf0cea
firasel <mdrasel601390@gmail.com>
2021-11-22 18:45:52 +0600

    cors alow

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(cookieParser());
 
 const userHandler = require("./routeHandler/userHandler");
