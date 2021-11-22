5c8923f54167de757368ffb30ca57a23cf16b1e4
firasel <mdrasel601390@gmail.com>
2021-11-23 01:37:58 +0600

    one user get

--- index.js
+++ index.js
@@ -17,19 +17,7 @@ app.use(
     sameSite: "None",
   })
 );
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", [
-    "http://localhost:3000",
-    "https://next-app-sandy-beta.vercel.app",
-    "https://next-jst-est-pied.vercel.app",
-  ]);
-  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+
 app.use(cookieParser());
 
 const userHandler = require("./routeHandler/userHandler");
