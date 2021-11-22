b601a5a77ea9e860e03279aec45777152d91bf6f
firasel <mdrasel601390@gmail.com>
2021-11-22 17:21:20 +0600

    cors allow

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors());
 app.use(cookieParser());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
