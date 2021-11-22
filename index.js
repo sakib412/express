f2ea67b21c1c0435b387312455f1bd9e9cbdde80
firasel <mdrasel601390@gmail.com>
2021-11-23 01:30:32 +0600

    one user get

--- index.js
+++ index.js
@@ -5,17 +5,24 @@ require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const app = express();
 app.use(express.json());
-
 app.use(
   cors({
-    origin: "*",
+    origin: [
+      "http://localhost:3000",
+      "https://next-app-sandy-beta.vercel.app",
+      "https://next-jst-est-pied.vercel.app",
+    ],
     credentials: true,
+    secure: true,
     sameSite: "None",
-    exposedHeaders: ["X-Set-Cookie"],
   })
 );
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", [
+    "http://localhost:3000",
+    "https://next-app-sandy-beta.vercel.app",
+    "https://next-jst-est-pied.vercel.app",
+  ]);
   res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
   res.header(
     "Access-Control-Allow-Headers",
