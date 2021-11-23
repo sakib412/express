860d9b59148b9d4ab41bf0e2566269e55e60fc96
firasel <mdrasel601390@gmail.com>
2021-11-23 19:31:38 +0600

    clear cookie api create

--- controller/User/Logout.js
+++ controller/User/Logout.js
@@ -2,12 +2,13 @@ const SendResponse = require("../SendResponse/SendResponse");
 
 const Logout = async (req, res) => {
   try {
-    res.clearCookie("jwt", {
-      path: "/",
-      domain: "user-taskapi.herokuapp.com",
-      sameSite: "none",
-      secure: true,
-    });
+    cookie = req.cookies;
+    for (let prop in cookie) {
+      if (!cookie.hasOwnProperty(prop)) {
+        continue;
+      }
+      res.cookie(prop, "", { expires: new Date(0) });
+    }
     res.status(200).send(SendResponse(true, "Cookie clear success"));
   } catch (err) {
     res.status(500).send(SendResponse(false, "Cookie clear not success"));
