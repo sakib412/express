5f93937ebcf70455eebeb74b32c18c8a189f6160
firasel <mdrasel601390@gmail.com>
2021-11-23 19:11:28 +0600

    clear cookie api create

--- controller/User/Logout.js
+++ controller/User/Logout.js
@@ -7,7 +7,7 @@ const Logout = async (req, res) => {
       domain: "user-taskapi.herokuapp.com",
       sameSite: "none",
       secure: true,
-      httpOnly: true,
+      expires:'Session'
     });
     res.status(200).send(SendResponse(true, "Cookie clear success"));
   } catch (err) {
